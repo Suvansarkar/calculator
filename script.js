edecd966f36cac6f78d0fa482620df668ed02603
Suvansarkar <suvansarkar1@gmail.com>
2023-10-04 14:11:44 +0530

    Added functionality for multiplication, substraction and division

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".number-button");
 let arr = [];
-let lastOperator = "";
+let lastOperator = "+";
 let dotUsed = false;
 
 let calculate = function (a, b, operator) {
     a = +a;
     b = +b;
     if(operator==="+"){
-        return a+b;
+        return +(a+b).toFixed(8);
     }else if(operator === "-"){
-        return a-b;
+        return +(a-b).toFixed(8);
     }else if(operator === "*"){
-        return a*b;
+        return +(a*b).toFixed(8);
     }else if(operator === "/"){
-        return a/b;
+        return +(a/b).toFixed(8);
     }
 
     alert("wtf? what did you just do?");
@@ -32,18 +32,56 @@ document.querySelector('.clear').addEventListener('click', ()=>{
     arr.forEach(item=>{item = 0;});
 });
 document.querySelector('.delete').addEventListener('click', ()=>{display.textContent = display.textContent.slice(0, -1); dotUsed=false;});
+
 document.querySelector('.add').addEventListener('click', ()=>{
-    lastOperator = "+";
     dotUsed=false;
-    if (display.textContent.includes("+")){
-        console.log("RUN!!!");
-        let arr = display.textContent.split("+");
+    if (display.textContent.includes(lastOperator)){
+        // console.log("RUN!!!");
+        let arr = display.textContent.split(lastOperator);
         display.textContent = calculate(arr[0], arr[1], lastOperator);
     }else{
         arr[0] = display.textContent;
     }
+    lastOperator = "+";
     display.textContent += "+";
 });
+document.querySelector('.multiply').addEventListener('click', ()=>{
+    dotUsed=false;
+    if (display.textContent.includes(lastOperator)){
+        // console.log("RUN!!!");
+        let arr = display.textContent.split(lastOperator);
+        display.textContent = calculate(arr[0], arr[1], lastOperator);
+    }else{
+        arr[0] = display.textContent;
+    }
+    lastOperator = "*";
+    display.textContent += "*";
+});
+document.querySelector('.divide').addEventListener('click', ()=>{
+    dotUsed=false;
+    if (display.textContent.includes(lastOperator)){
+        // console.log("RUN!!!");
+        let arr = display.textContent.split(lastOperator);
+        display.textContent = calculate(arr[0], arr[1], lastOperator);
+    }else{
+        arr[0] = display.textContent;
+    }
+    lastOperator = "/";
+    display.textContent += "/";
+});
+document.querySelector('.subtract').addEventListener('click', ()=>{
+    dotUsed=false;
+    if (display.textContent.includes(lastOperator)){
+        // console.log("RUN!!!");
+        let arr = display.textContent.split(lastOperator);
+        display.textContent = calculate(arr[0], arr[1], lastOperator);
+    }else{
+        arr[0] = display.textContent;
+    }
+    lastOperator = "-";
+    display.textContent += "-";
+});
+
 document.querySelector('.dot').addEventListener('click', ()=> {
     if(dotUsed==false){
         dotUsed=true;
@@ -54,5 +92,7 @@ document.querySelector('.equals').addEventListener('click', ()=>{
     dotUsed=false;
     let arr = display.textContent.split(lastOperator);
     display.textContent = calculate(arr[0], arr[1], lastOperator);
-    a = +display.textContent;
+    if(display.textContent.includes('.')){
+        dotUsed = true;
+    }
 });
